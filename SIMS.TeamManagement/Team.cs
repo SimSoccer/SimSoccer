8304bdeb2a2663ef45a6fe27190016a93d3695dc
thibaud <pernet-bissier@intechinfo.fr>
2014-11-17 14:39:44 +0100

    take data from xml

--- SIMS.TeamManagement/Team.cs
+++ SIMS.TeamManagement/Team.cs
@@ -3,13 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
-namespace SoccerSimulator
+namespace SIMS.PlayerManagement
 {
-    class Team
+    class TeamsList
     {
-        
+        Dictionary<string, Team> _teams;
+        Team t;
+        public TeamsList()
+        {
+            _teams = new Dictionary<string, Team>();
+        }
 
+        public Team CreatePlayerList()
+        {
 
+            var doc = XDocument.Load( @"C:\Users\famille\Documents\GitHub\SimSoccer\Ligue1Teams.xml" );
+
+            var players = doc.Descendants( "Team" ).Select( t1 => new Team
+            {
+                Id = int.Parse(team.Element("Id").Value),
+            } ).ToDictionary< teams => team.Id);
+
+            return t;
+        }
+
+        public Dictionary<string, Teams> Name
+        {
+            get { return _teams; }
+        }
     }
 }
