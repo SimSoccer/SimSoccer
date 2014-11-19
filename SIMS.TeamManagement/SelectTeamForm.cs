0494ae01c513436bce3c60ca8f6117f6ec570d81
thibaud <pernet-bissier@intechinfo.fr>
2014-11-19 11:46:26 +0100

    create winform

---
+++ SIMS.TeamManagement/SelectTeamForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SIMS.TeamsManagement
+{
+    public partial class SelectTeamForm : Form
+    {
+        public SelectTeamForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
