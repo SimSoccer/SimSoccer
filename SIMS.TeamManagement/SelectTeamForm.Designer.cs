a63839a01f63e28bb2fcec8249fc0158ace95c94
thibaud <pernet-bissier@intechinfo.fr>
2014-11-19 16:33:37 +0100

    big updat

--- SIMS.TeamManagement/SelectTeamForm.Designer.cs
+++ SIMS.TeamManagement/SelectTeamForm.Designer.cs
@@ -30,26 +30,71 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SelectTeamForm));
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Logo = new System.Windows.Forms.PictureBox();
+            this.teamName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Logo)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
             resources.ApplyResources(this.button1, "button1");
             this.button1.Name = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // Logo
+            // 
+            resources.ApplyResources(this.Logo, "Logo");
+            this.Logo.Name = "Logo";
+            this.Logo.TabStop = false;
+            this.Logo.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // teamName
+            // 
+            resources.ApplyResources(this.teamName, "teamName");
+            this.teamName.Name = "teamName";
+            this.teamName.TextChanged += new System.EventHandler(this.teamName_TextChanged);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
             // 
             // SelectTeamForm
             // 
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.teamName);
+            this.Controls.Add(this.Logo);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "SelectTeamForm";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.SelectTeamForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Logo)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox Logo;
+        protected System.Windows.Forms.TextBox teamName;
+        private System.Windows.Forms.Label label1;
     }
 }
--- SIMS.TeamManagement/SelectTeamForm.cs
+++ SIMS.TeamManagement/SelectTeamForm.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,34 @@ namespace SIMS.TeamsManagement
         public SelectTeamForm()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click( object sender, EventArgs e )
+        {
+
+        }
+
+        private void SelectTeamForm_Load( object sender, EventArgs e )
+        {
+
+        }
+
+        private void pictureBox1_Click( object sender, EventArgs e )
+        {
+            using( FileStream fs = new FileStream( @"C:\Users\famille\Pictures\Sans titre.png", FileMode.Open ) )
+            {
+                Logo.Image = Image.FromStream( fs );
+                teamName_TextChanged( this, EventArgs.Empty );
+            }
+            
+            
+        }
+
+        private void teamName_TextChanged( object sender, EventArgs e )
+        {
+            teamName.Text = "équipe de merde";
+            
+
         }
     }
 }
