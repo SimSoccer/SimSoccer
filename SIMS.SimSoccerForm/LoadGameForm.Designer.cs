15c1ac6f2a77525c202538b572180556a4a25e30
flo <cuchot@intechinfo.fr>
2014-12-03 09:49:20 +0100

    Update of StartForm and add new form LoadGameForm

---
+++ SIMS.SimSoccerForm/LoadGameForm.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace SIMS.SimSoccerForm
+{
+    partial class LoadGameForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose( bool disposing )
+        {
+            if( disposing && (components != null) )
+            {
+                components.Dispose();
+            }
+            base.Dispose( disposing );
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoadGameForm));
+            this.btLoadGame = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btLoadGame
+            // 
+            this.btLoadGame.Location = new System.Drawing.Point(171, 69);
+            this.btLoadGame.Name = "btLoadGame";
+            this.btLoadGame.Size = new System.Drawing.Size(75, 23);
+            this.btLoadGame.TabIndex = 0;
+            this.btLoadGame.Text = "Load";
+            this.btLoadGame.UseVisualStyleBackColor = true;
+            // 
+            
+            // LoadGameForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btLoadGame);
+            this.Name = "LoadGameForm";
+            this.Text = "LoadGameForm";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btLoadGame;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
---
+++ SIMS.SimSoccerForm/LoadGameForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Sims.SimSoccerModel;
+
+namespace SIMS.SimSoccerForm
+{
+    public partial class LoadGameForm : Form
+    {
+        readonly Game _game;
+        List<string> _items = new List<string>();
+        public LoadGameForm()
+        {
+            InitializeComponent();
+            _items.Add( "One" ); 
+            _items.Add( "Two" );
+            _items.Add( "Three" );
+
+            listBox1.DataSource = _items;
+
+           
+        }
+
+       
+    }
+}
