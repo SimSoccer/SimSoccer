bd1fd2808a9164b249b93079861b7e919582c09f
DONNE Loic <loic.donne95@gmail.com>
2015-01-27 07:01:49 +0100

    test Unitaires

---
+++ SIMS.SimSoccerTests/LigueTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using Sims.SimSoccerModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIMS.SimSoccerTests
+{
+    public class LigueTests
+    {
+        [Test]
+
+        public void a_calendar_have_38_dayMatch()
+        {
+            Game g = new Game("test", "1234", "blabla", "lala");
+            int _year = 2014;
+            Ligue _ligue1 = new Ligue(g, _year);
+            Calendar _calendar = new Calendar(g.TeamList.Teams.Count, _year, _ligue1);
+
+            Assert.That(_calendar.MatchDay.Count == 38);
+        }
+
+        [Test]
+        public void ligue_contains_20_teams()
+        {
+            Game g = new Game("test", "1234", "blabla", "lala");
+            Assert.That(g.TeamList.Teams.Count == 20);
+        }
+    }
+}
---
+++ SIMS.SimSoccerTests/TacticTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using Sims.SimSoccerModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIMS.SimSoccerTests
+{
+     public class TacticTests
+    {
+         [Test]
+
+         public void team_list_titulaire_is_equal_to_11()
+         {
+             Game g = new Game("test", "1234", "blabla", "lala");
+             int i = 0;
+             Assert.That(g.TeamList.Teams[i].TeamType.Count == 11);
+         }
+
+         [Test]
+         public void team_list_remplacent_is_equal_to_7()
+         {
+             Game g = new Game("test", "1234", "blabla", "lala");
+             int i = 0;
+             Assert.That(g.TeamList.Teams[i].Remplacent.Count == 7);
+         }
+    }
+}
